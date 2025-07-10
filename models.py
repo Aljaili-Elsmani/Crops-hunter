39e3f2847941a3ee06bc69e82194f08fbce823bb
Aljaili-Elsmani <jailanismani@gmail.com>
2025-07-10 15:42:20 +0400

    Update models.py

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ class Product(db.Model):
     category = db.Column(db.String(50), nullable=False)
     price = db.Column(db.Float, nullable=False)
     unit = db.Column(db.String(20), nullable=False)
-    origin = db.Column(db.String(100))       # مكان الإنتاج
-    quantity = db.Column(db.String(50))      # الكمية المتوفرة
-    notes = db.Column(db.Text)                # ملاحظات إضافية
+    origin = db.Column(db.String(100))
+    quantity = db.Column(db.String(50))
+    notes = db.Column(db.Text)
 
     def __repr__(self):
         return f'<Product {self.name}>'
