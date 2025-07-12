439b1aa66a18175f39df61d89c023535431ab2b0
Aljaili-Elsmani <jailanismani@gmail.com>
2025-07-12 20:30:15 +0400

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,11 +7,10 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(50), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    unit = db.Column(db.String(20), nullable=False)
-    origin = db.Column(db.String(100))
+    price = db.Column(db.String(20), nullable=False)
+    unit = db.Column(db.String(20))
     quantity = db.Column(db.String(50))
-    notes = db.Column(db.Text)
-
-    def __repr__(self):
-        return f'<Product {self.name}>'
+    origin = db.Column(db.String(100))
+    production_date = db.Column(db.String(7))  # صيغة: YYYY-MM
+    image_filename = db.Column(db.String(200))  # لتخزين اسم الصورة
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
